f0332c42fb51505eb4b7bdb1cd66c9d853586b08
slashfly <slashfly2@gmail.com>
2022-12-30 22:20:06 -0700

    Fix indentation

--- SalamanderBot/src/main/java/com/github/slashfly/salamanderbot/SalamanderBot.java
+++ SalamanderBot/src/main/java/com/github/slashfly/salamanderbot/SalamanderBot.java
@@ -1,6 +1,6 @@
 package com.github.slashfly.salamanderbot;
 
-import org.reactivestreams.Publisher;		
+import org.reactivestreams.Publisher;
 
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
@@ -18,44 +18,43 @@ import reactor.core.publisher.Mono;
 public class SalamanderBot {
 
     public static void main(String[] args) {
-		
-            String token = "";
-            
-            // login
-            GatewayDiscordClient client = DiscordClientBuilder.create(token).build().login().block();
-            
-            // obtain commands from json files
-	    List<String> commands = List.of("roll.json");
-            try {
-                new GlobalCommandRegistrar(client.getRestClient()).registerCommands(commands);
-            } catch (Exception e) {
+        String token = "";
+
+        // login
+        GatewayDiscordClient client = DiscordClientBuilder.create(token).build().login().block();
+
+        // obtain commands from json files
+        List<String> commands = List.of("roll.json");
+        try {
+            new GlobalCommandRegistrar(client.getRestClient()).registerCommands(commands);
+        } catch (Exception e) {
+        }
+
+        client.on(new ReactiveEventAdapter() {
+            @Override
+            public Publisher<?> onChatInputInteraction(ChatInputInteractionEvent event) {
+                // get the user initating the interaction
+                final User author = event.getInteraction().getUser();
+                // all command responses
+                if (event.getCommandName().equals("roll")) {
+                    String roll = roll(event.getInteraction().getCommandInteraction().get());
+                    return event.reply(author.getMention() + ", You rolled **" + roll + "**!");
+                }
+                return Mono.empty();
             }
- 
-	    client.on(new ReactiveEventAdapter() {
-                    @Override
-	    	public Publisher<?> onChatInputInteraction(ChatInputInteractionEvent event) {
-                    // get the user initating the interaction
-                    final User author = event.getInteraction().getUser();
-                        // all command responses
-	    		if (event.getCommandName().equals("roll")) {
-                            String roll = roll(event.getInteraction().getCommandInteraction().get());
-                            return event.reply(author.getMention() + ", You rolled **" + roll + "**!");
-	    		}
-	    		return Mono.empty();
-	    	}
-	    }).blockLast();
-	}
-	
-        // roll command
-	private static String roll(ApplicationCommandInteraction acid) {
-                // get the max number that was picked, if no number was chosen then default to 100
-		long max = acid.getOption("max")
-                    .flatMap(ApplicationCommandInteractionOption::getValue)
-                    .map(ApplicationCommandInteractionOptionValue::asLong)
-                    .orElse(100L);
-                // generate random number
-		StringBuilder result = new StringBuilder();
-		result.append((int) ((Math.random() * (max - 1)) + 1));
-		return result.toString();
-	}
+        }).blockLast();
+    }
+
+    // roll command
+    private static String roll(ApplicationCommandInteraction acid) {
+        // get the max number that was picked, if no number was chosen then default to 100
+        long max = acid.getOption("max")
+                .flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asLong)
+                .orElse(100L);
+        // generate random number
+        StringBuilder result = new StringBuilder();
+        result.append((int) ((Math.random() * (max - 1)) + 1));
+        return result.toString();
+    }
 }
