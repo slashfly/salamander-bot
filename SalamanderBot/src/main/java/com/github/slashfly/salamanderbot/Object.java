31f60f2a866d3c798fbf55e70901734da6fd8950
Sean <Sean@10.0.0.126>
2023-01-09 09:44:47 -0700

    Implementation of objects for blackjack

---
+++ SalamanderBot/src/main/java/com/github/slashfly/salamanderbot/Object.java
@@ -0,0 +1,13 @@
+package com.github.slashfly.salamanderbot;
+
+public class Object {
+    private int hand;
+    
+    public int getHand(){
+        return this.hand;
+    }
+    
+    public void setHand(int newHand){
+        this.hand = newHand;
+    }
+}
