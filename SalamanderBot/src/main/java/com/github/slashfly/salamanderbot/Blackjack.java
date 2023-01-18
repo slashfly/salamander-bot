265fceadcb10662cbfe2d5994da62291640e9cd2
slashfly <slashfly2@gmail.com>
2023-01-18 09:54:48 -0700

    Change methods to public so they can be referred to

--- SalamanderBot/src/main/java/com/github/slashfly/salamanderbot/Blackjack.java
+++ SalamanderBot/src/main/java/com/github/slashfly/salamanderbot/Blackjack.java
@@ -7,8 +7,8 @@ public class Blackjack {
 
     public static ArrayList<Object> player = new ArrayList<Object>();
     public static ArrayList<Object> dealer = new ArrayList<Object>();
-    
-    private static String dealer(ApplicationCommandInteraction acid) {
+
+    public static String dealer(ApplicationCommandInteraction acid) {
         dealer.add(new Object());
         int cards = 0;
 
@@ -27,37 +27,37 @@ public class Blackjack {
         return dealerCards.toString();
     }
 
-    private static String hit(ApplicationCommandInteraction acid) {
+    public static String hit(ApplicationCommandInteraction acid) {
         player.get(0).setHand(player.get(0).getHand() + (int) ((Math.random() * (11 - 1)) + 1));
-        
+
         StringBuilder playerCards = new StringBuilder();
         playerCards.append(player.get(0).getHand());
         return playerCards.toString();
     }
 
-    private static String stand(ApplicationCommandInteraction acid,  int dealerHand) {
+    public static String stand(ApplicationCommandInteraction acid, int dealerHand) {
         StringBuilder result = new StringBuilder();
-        
+
         // result 1: dealer had more cards than player and didn't bust
-        if(dealer.get(0).getHand() > player.get(0).getHand() && dealer.get(0).getHand() < 22){
+        if (dealer.get(0).getHand() > player.get(0).getHand() && dealer.get(0).getHand() < 22) {
             result.append(1);
-        // result 2: player busted
-        } else if(player.get(0).getHand() > 21) {
+            // result 2: player busted
+        } else if (player.get(0).getHand() > 21) {
             result.append(2);
-        // result 3: both busted
-        } else if(player.get(0).getHand() > 21 && dealer.get(0).getHand() > 21){
+            // result 3: both busted
+        } else if (player.get(0).getHand() > 21 && dealer.get(0).getHand() > 21) {
             result.append(3);
-        // result 4: same amount of cards
-        } else if(player.get(0).getHand() == dealer.get(0).getHand()){
+            // result 4: same amount of cards
+        } else if (player.get(0).getHand() == dealer.get(0).getHand()) {
             result.append(4);
-        // result 5: dealer had a total of 21 and player did not
-        } else if(dealer.get(0).getHand() == 21 && player.get(0).getHand() != 21){
+            // result 5: dealer had a total of 21 and player did not
+        } else if (dealer.get(0).getHand() == 21 && player.get(0).getHand() != 21) {
             result.append(5);
-        // result 6: player had a total of 21 and dealer did not
-        } else if(player.get(0).getHand() == 21 && dealer.get(0).getHand() != 21){
+            // result 6: player had a total of 21 and dealer did not
+        } else if (player.get(0).getHand() == 21 && dealer.get(0).getHand() != 21) {
             result.append(6);
         }
-        
+
         dealer.remove(0);
         player.remove(0);
         return result.toString();
